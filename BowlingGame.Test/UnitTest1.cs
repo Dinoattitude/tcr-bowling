197efad9057ad3d115134aa09eb6c1a36591ed6c
Yolowix1 <mlaborde312@gmail.com>
2022-10-20 15:11:55 +0200

    v1

--- BowlingGame.Test/UnitTest1.cs
+++ BowlingGame.Test/UnitTest1.cs
@@ -6,8 +6,172 @@ namespace BowlingGame.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test2Quilles()
         {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 2 quilles
+            partie.Lancer(2);
+
+            // ALORS le score est de 0
+            Assert.AreEqual(0, partie.Score);
+        }
+
+
+        [TestMethod]
+        public void Test2Lancers1Quille()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 1 quilles deux fois
+            partie.Lancer(1);
+            partie.Lancer(1);
+
+            // ALORS le score est de 2
+            Assert.AreEqual(2, partie.Score);
+        }
+
+        [TestMethod]
+        public void Test1Quille()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber une quille
+            partie.Lancer(1);
+
+            // ALORS le score est de 0 car 1 seul lancer
+            Assert.AreEqual(0, partie.Score);
+        }
+
+        [TestMethod]
+        public void TestLancerRaté()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 0 quilles
+            partie.Lancer(0);
+
+            // ALORS le score est de 0
+            Assert.AreEqual(0, partie.Score);
+        }
+
+        [TestMethod]
+        public void TestStrike()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 10 quilles
+            partie.Lancer(10);
+
+            // ALORS le score est de 0
+            Assert.AreEqual(0, partie.Score);
+        }
+
+        [TestMethod]
+        public void TestSpare()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 10 quilles en 2 fois
+            partie.Lancer(9);
+            partie.Lancer(1);
+
+            // ALORS le score est de 0
+            Assert.AreEqual(0, partie.Score);
+        }
+
+        [TestMethod]
+        public void Test9Carreau()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 9 carreau avec 2 Quilles à chaque fois
+            int i = 1;
+            do
+            {
+                i++;
+                partie.Lancer(1);
+                partie.Lancer(1);
+            } while (i < 10);
+
+            // ALORS le score est de 0
+            Assert.AreEqual(18, partie.ScoreFinal);
+        }
+
+        [TestMethod]
+        public void Test10CarreauStrike()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 9 carreau avec 2 Quilles à chaque fois
+            int i = 1;
+            do
+            {
+                i++;
+                partie.Lancer(1);
+                partie.Lancer(1);
+            } while (i < 10);
+
+            partie.Lancer(10);
+            partie.Lancer(3);
+            partie.Lancer(2);
+
+            // ALORS le score est de 0
+            Assert.AreEqual(33, partie.ScoreFinal);
+        }
+
+        [TestMethod]
+        public void Test10CarreauSpare()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 9 carreau avec 2 Quilles à chaque fois
+            int i = 1;
+            do
+            {
+                i++;
+                partie.Lancer(1);
+                partie.Lancer(1);
+            } while (i < 10);
+
+            partie.Lancer(5);
+            partie.Lancer(5);
+            partie.Lancer(5);
+    
+            // ALORS le score est de 0
+            Assert.AreEqual(33, partie.ScoreFinal);
+        }
+
+        [TestMethod]
+        public void Test10Carreau3Strikes()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 9 carreau avec 2 Quilles à chaque fois
+            int i = 1;
+            do
+            {
+                i++;
+                partie.Lancer(1);
+                partie.Lancer(1);
+            } while (i < 10);
+
+            partie.Lancer(10);
+            partie.Lancer(10);
+            partie.Lancer(10);
+
+            // ALORS le score est de 0
+            Assert.AreEqual(48, partie.ScoreFinal);
         }
     }
 }
---
+++ BowlingGame/Partie.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace BowlingGame
+{
+    public class Partie
+    {
+        private readonly List<int> _historique = new();
+
+        public void Lancer(int nombreQuillesTombées)
+        {
+            _historique.Add(nombreQuillesTombées);
+
+            if (EstLaDerniereFrame()) return;
+            if (EstUnStrike()) return;
+            if (EstUnSpare()) return;
+
+            if (TermineUneFrame())
+            {
+                Score = DeuxDerniersLancers.Sum();
+                ScoreFinal += Score;
+                nbFrame++;
+            }
+        }
+
+        private IEnumerable<int> DeuxDerniersLancers => _historique.AsEnumerable().Reverse().Take(2);
+        private bool TermineUneFrame() => _historique.Count % 2 == 0;
+        private bool EstUnSpare() => DeuxDerniersLancers.Sum() == 10;
+        private bool EstUnStrike() => _historique.Last() == 10;
+        private bool EstLaDerniereFrame() => nbFrame == 9 ? true : false;
+
+        public int Score { get; private set; }
+        public int ScoreFinal { get; private set; }
+        public int nbFrame { get; private set; } = 0;
+    }
+}
